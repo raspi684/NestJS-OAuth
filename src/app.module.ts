b86d4183abf38493a1a001fd9ae424b585ca5066
Raspi684 <kontakt@dominikwawrzyncza.pl>
2020-08-21 22:16:08 +0200

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
